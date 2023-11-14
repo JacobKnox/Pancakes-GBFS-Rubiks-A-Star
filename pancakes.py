3523fddfe57080a5080455a8bbecb5da31b1358b
Jacob Knox <jacobknoxa@gmail.com>
2023-11-14 17:30:48 -0500

    Pancakes show now

--- pancakes.py
+++ pancakes.py
@@ -9,9 +9,16 @@ from queue import PriorityQueue
 import random
 import time
 
-parser = argparse.ArgumentParser(description="Use greedy best-first search (GBFS) to optimally flip a stack of pancakes")
-parser.add_argument('-n', '--num', metavar='pancakes', type=int, help="number of pancakes", default=8)
-parser.add_argument('--seed', type=int, help="seed for randomly arranging pancakes initially")
+parser = argparse.ArgumentParser(
+    description="Use greedy best-first search (GBFS) to optimally flip a stack of pancakes"
+)
+parser.add_argument(
+    "-n", "--num", metavar="pancakes", type=int, help="number of pancakes", default=8
+)
+parser.add_argument(
+    "--seed", type=int, help="seed for randomly arranging pancakes initially"
+)
+
 
 def main(args):
     # Parse inputs
@@ -29,17 +36,20 @@ def main(args):
         if key:
             if key == "Escape":  # quit the program
                 break
-            elif key == 'd':  # debug the program
+            elif key == "d":  # debug the program
                 pdb.set_trace()
-            elif key == 'g':  # run greedy best-first search
+            elif key == "g":  # run greedy best-first search
                 path = gbfs(gui, stack)
-            elif key in [str(i) for i in range(1, n + 1)]:  # manually flip some of the pancakes
+            elif key in [
+                str(i) for i in range(1, n + 1)
+            ]:  # manually flip some of the pancakes
                 flip(gui, stack, int(key))
 
     gui.close()
 
+
 def guisetup(stack):
-    '''Create graphical user interface for a stack of n pancakes.'''
+    """Create graphical user interface for a stack of n pancakes."""
     n = len(stack)  # number of pancakes in the stack
     thickness = 12  # thickness of each pancake, in pixels
     margin = 40
@@ -48,13 +58,23 @@ def guisetup(stack):
     gui = GraphWin("Pancakes", wid, hei)
 
     cx = wid / 2  # center of width
-    cmap = cm.get_cmap('YlOrBr', n + 1)
+    cmap = cm.get_cmap("YlOrBr", n + 1)
 
     # Draw pancakes
     # ***ENTER CODE HERE*** (10 lines)
+    for i in range(1, n + 1):
+        pan_wid = 30 * i
+        pancake = Rectangle(
+            Point(cx - (pan_wid / 2), margin + (thickness * (i - 1))),
+            Point(cx + (pan_wid / 2), margin + (thickness * i)),
+        )
+        pancake.draw(gui)
 
     # Add text objects for instructions and status updates
-    instructions = Text(Point(10, hei - 12), "Press a # to flip pancakes, 'g' to run GBFS, Escape to quit")
+    instructions = Text(
+        Point(10, hei - 12),
+        "Press a # to flip pancakes, 'g' to run GBFS, Escape to quit",
+    )
     instructions._reconfig("anchor", "w")
     instructions.setSize(8)
     instructions.draw(gui)
@@ -67,8 +87,9 @@ def guisetup(stack):
     # Return gui object
     return gui
 
+
 def flip(gui, stack, p):
-    '''Flip p pancakes in an ordered stack.'''
+    """Flip p pancakes in an ordered stack."""
     # print("Flipping", p, "pancakes" if p > 1 else "pancake")
 
     # Get graphics objects from GUI
@@ -88,15 +109,17 @@ def flip(gui, stack, p):
 
     return stack
 
+
 def cost(stack):
-    '''Compute the cost h(stack) for a given stack of pancakes.
-    Here, we define cost as the number of pancakes in the wrong position.'''
+    """Compute the cost h(stack) for a given stack of pancakes.
+    Here, we define cost as the number of pancakes in the wrong position."""
     # ***MODIFY CODE HERE*** (2 lines)
     h = 0
     return h
 
+
 def gbfs(gui, stack):
-    '''Run greedy best-first search on a stack of pancakes and return the solution path.'''
+    """Run greedy best-first search on a stack of pancakes and return the solution path."""
     print("Running greedy best-first search...")
 
     # Get graphics objects from GUI
@@ -111,22 +134,27 @@ def gbfs(gui, stack):
     # ***MODIFY CODE HERE*** (20-25 lines)
     cnt = 0
 
-    print(f'searched {cnt} paths')
-    print('solution:', '')
+    print(f"searched {cnt} paths")
+    print("solution:", "")
     status.setText("...search is complete")
 
+
 def simulate(stack, path):
-    '''Simulate the flipping of pancakes to determine the resulting stack.'''
+    """Simulate the flipping of pancakes to determine the resulting stack."""
     fakestack = stack.copy()  # make a copy so we don't actually change the real stack
     for action in path:
         try:
             p = int(action)  # how many pancakes are we trying to flip?
             for i in range(1, p // 2 + 1):
-                fakestack[-i], fakestack[- (p - i + 1)] = fakestack[-(p - i + 1)], fakestack[-i]
+                fakestack[-i], fakestack[-(p - i + 1)] = (
+                    fakestack[-(p - i + 1)],
+                    fakestack[-i],
+                )
         except:
             print("INVALID ACTION: Check code")
 
     return fakestack
+
 
 if __name__ == "__main__":
     main(parser.parse_args())
