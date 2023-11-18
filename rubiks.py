aa60766824ed4bd3040490707ac96bc40e46beb9
Jacob Knox <jacobknoxa@gmail.com>
2023-11-17 19:16:54 -0500

    A* works

--- rubiks.py
+++ rubiks.py
@@ -7,20 +7,21 @@ import pdb
 from queue import PriorityQueue
 
 parser = argparse.ArgumentParser(description="Solving a Rubik's Cube with A* Search")
-parser.add_argument('-s', '--state', help="text file containing initial state of the cube, encoded as a sequence of integers")
+parser.add_argument(
+    "-s",
+    "--state",
+    help="text file containing initial state of the cube, encoded as a sequence of integers",
+)
+
 
 def main(args):
     # Initialize dictionary of parameters
     params = {
-        'colors': ["#b71234",
-                   "#0046ad",
-                   "#ffffff",
-                   "#009b48",
-                   "#ffd500",
-                   "#ff5800"],
-        'n': 3,
-        'pixels': 45,
-        'thickness': 4}
+        "colors": ["#b71234", "#0046ad", "#ffffff", "#009b48", "#ffd500", "#ff5800"],
+        "n": 3,
+        "pixels": 45,
+        "thickness": 4,
+    }
 
     # Make list to store individual square colors
     # HINT: If the user entered an initial state via command line argument, you
@@ -29,7 +30,10 @@ def main(args):
     # ***MODIFY CODE HERE*** (7 lines)
     current_state = []
     for i in range(6):
-        current_state += [i] * params['n'] ** 2
+        current_state += [i] * params["n"] ** 2
+    if args.state:
+        with open(args.state) as file:
+            current_state = [int(num) for num in list(file.readline())]
 
     # ***DO NOT MODIFY THE FOLLOWING 2 LINES***
     initial_state = current_state.copy()  # for resetting the cube
@@ -47,79 +51,108 @@ def main(args):
             if key == "Escape":  # quit the program
                 break
 
-            elif key == 'p':  # debug the program
+            elif key == "p":  # debug the program
                 pdb.set_trace()
 
             elif key == "Ctrl+r":
                 # Reset the cube to its initial state
-                print('Resetting cube to initial state')
+                print("Resetting cube to initial state")
                 current_state = initial_state.copy()
                 previous_state = initial_state.copy()
                 recolor(gui, current_state, params)
 
             elif key == "Ctrl+z":
                 # Undo the last user action
-                print('Undoing last user action')
+                print("Undoing last user action")
                 current_state = previous_state.copy()
                 recolor(gui, current_state, params)
 
-            elif key.upper() in 'UDLRBF':
+            elif key.upper() in "UDLRBF":
                 # Rotate one of the cube faces clockwise
                 previous_state = current_state.copy()
                 face = key.upper()
-                direction = 'CW'
+                direction = "CW"
                 print("Rotating", face, "face", direction)
                 txt = gui.items[-1]
                 txt.setText("Rotating " + face + " face " + direction)
                 rotate(current_state, face, direction)
                 recolor(gui, current_state, params)
 
-            elif key[:6] == 'Shift+' and key[6].upper() in 'UDLRBF':
+            elif key[:6] == "Shift+" and key[6].upper() in "UDLRBF":
                 # Rotate one of the cube faces counterclockwise
                 previous_state = current_state.copy()
                 face = key[6].upper()
-                direction = 'CCW'
+                direction = "CCW"
                 print("Rotating", face, "face", direction)
                 txt = gui.items[-1]
                 txt.setText("Rotating " + face + " face " + direction)
                 rotate(current_state, face, direction)
                 recolor(gui, current_state, params)
 
-            elif key == 'a':
+            elif key == "a":
                 # Solve the cube using A* search
                 path = astar(current_state)
 
-            elif key == 'h':
+            elif key == "h":
                 # Print the current heuristic cost
                 print(f"Current heuristic cost = {cost('', current_state)}")
 
     gui.close()
 
+
 def astar(state, verbose=False):
-    '''Run A* search on the cube based on its current state and return the solution path.'''
-    print('Running A* search...')
+    """Run A* search on the cube based on its current state and return the solution path."""
+    print("Running A* search...")
     # ***ENTER CODE HERE*** (20-25 lines)
     cnt = 0
+    pq = PriorityQueue()
+    pq.put((cost("", state), ""))
+    solution = []
+    visited = []
+    while not pq.empty():
+        node = pq.get()
+        temp_state = simulate(state, node[1])
+        visited.append(temp_state)
+        if verbose:
+            print(f"Looking at path {node[1]}")
+        cnt += 1
+        if cost("", temp_state) > 0:
+            for i in "UuDdLlRrBbFf":
+                if (node[1] == "" or node[1][-1] != i.swapcase()) and (
+                    simulate(temp_state, i) not in visited
+                ):
+                    pq.put((cost(node[1] + i, simulate(temp_state, i)), node[1] + i))
+        else:
+            solution = node[1]
+            break
 
-    print(f'searched {cnt} paths')
-    print('solution:', '')
+    print(f"searched {cnt} paths")
+    print("solution:", solution)
+    return solution
+
 
 def cost(node, state):
-    '''Compute the cost g(node)+h(node) for a given set of moves (node) leading to a cube state.
+    """Compute the cost g(node)+h(node) for a given set of moves (node) leading to a cube state.
     Let g(node) be the number of moves it took to get to the state.
     Let h(node) be the average number of incorrect square colors on the cube. For h(node)=0, all colors will match the center color of that face, which never moves.
-    '''
+    """
 
     # ***MODIFY CODE HERE*** (1 line)
-    g = 0
+    g = len(node)
 
     # ***MODIFY CODE HERE*** (7 lines)
     h = 0
+    for i in range(4, len(state), 9):
+        for j in range(i - 4, i + 4):
+            if state[j] != state[i]:
+                h += 1
+    h = h / 6
 
     return g + h
 
+
 def drawface(gui, x0, y0, c, n, w, t):
-    '''Draw an individual face of the cube. Requires GraphWin object, starting (x,y) position of the top-left corner of the face, face color, number of squares per row/column, pixel width of each square, and border thickness.'''
+    """Draw an individual face of the cube. Requires GraphWin object, starting (x,y) position of the top-left corner of the face, face color, number of squares per row/column, pixel width of each square, and border thickness."""
     for i in range(n):
         for j in range(n):
             x = x0 + j * w
@@ -129,14 +162,15 @@ def drawface(gui, x0, y0, c, n, w, t):
             square.setWidth(t)
             square.draw(gui)
 
+
 def guisetup(params):
-    '''Create graphical user interface for Rubik's Cube with n rows and columns.'''
+    """Create graphical user interface for Rubik's Cube with n rows and columns."""
 
     # Extract relevant parameters
-    n = params['n']
-    clr = params['colors']
-    px = params['pixels']
-    t = params['thickness']
+    n = params["n"]
+    clr = params["colors"]
+    px = params["pixels"]
+    t = params["thickness"]
 
     # Draw graphics window
     wid = (4 * n + 2) * px  # +2 for the margin
@@ -152,7 +186,9 @@ def guisetup(params):
     drawface(gui, (n + 1) * px, (2 * n + 1) * px, clr[5], n, px, t)  # down
 
     # Add text instructions
-    txt = Text(Point(15, 20), "Press U/D/L/R/B/F to rotate a cube face CW (hold Shift for CCW)")
+    txt = Text(
+        Point(15, 20), "Press U/D/L/R/B/F to rotate a cube face CW (hold Shift for CCW)"
+    )
     txt._reconfig("anchor", "w")
     txt.setSize(12)
     txt.draw(gui)
@@ -167,76 +203,439 @@ def guisetup(params):
     # Return gui object and list of cube square color indices
     return gui
 
-def rotate(state, face, direction='CW'):
-    '''Rotate the cube face (U/D/L/R/B/F) in a given direction (CW/CCW).'''
-    if face == 'U':
+
+def rotate(state, face, direction="CW"):
+    """Rotate the cube face (U/D/L/R/B/F) in a given direction (CW/CCW)."""
+    if face == "U":
         src = [9, 10, 11, 18, 19, 20, 27, 28, 29, 36, 37, 38, 0, 1, 2, 5, 8, 7, 6, 3]
-        if direction == 'CW':
-            dst = [36, 37, 38, 9, 10, 11, 18, 19, 20, 27, 28, 29, 2, 5, 8, 7, 6, 3, 0, 1]
-        elif direction == 'CCW':
-            dst = [18, 19, 20, 27, 28, 29, 36, 37, 38, 9, 10, 11, 6, 3, 0, 1, 2, 5, 8, 7]
+        if direction == "CW":
+            dst = [
+                36,
+                37,
+                38,
+                9,
+                10,
+                11,
+                18,
+                19,
+                20,
+                27,
+                28,
+                29,
+                2,
+                5,
+                8,
+                7,
+                6,
+                3,
+                0,
+                1,
+            ]
+        elif direction == "CCW":
+            dst = [
+                18,
+                19,
+                20,
+                27,
+                28,
+                29,
+                36,
+                37,
+                38,
+                9,
+                10,
+                11,
+                6,
+                3,
+                0,
+                1,
+                2,
+                5,
+                8,
+                7,
+            ]
 
-    elif face == 'D':
-        src = [45, 46, 47, 50, 53, 52, 51, 48, 15, 16, 17, 24, 25, 26, 33, 34, 35, 42, 43, 44]
-        if direction == 'CW':
-            dst = [47, 50, 53, 52, 51, 48, 45, 46, 24, 25, 26, 33, 34, 35, 42, 43, 44, 15, 16, 17]
-        elif direction == 'CCW':
-            dst = [51, 48, 45, 46, 47, 50, 53, 52, 42, 43, 44, 15, 16, 17, 24, 25, 26, 33, 34, 35]
+    elif face == "D":
+        src = [
+            45,
+            46,
+            47,
+            50,
+            53,
+            52,
+            51,
+            48,
+            15,
+            16,
+            17,
+            24,
+            25,
+            26,
+            33,
+            34,
+            35,
+            42,
+            43,
+            44,
+        ]
+        if direction == "CW":
+            dst = [
+                47,
+                50,
+                53,
+                52,
+                51,
+                48,
+                45,
+                46,
+                24,
+                25,
+                26,
+                33,
+                34,
+                35,
+                42,
+                43,
+                44,
+                15,
+                16,
+                17,
+            ]
+        elif direction == "CCW":
+            dst = [
+                51,
+                48,
+                45,
+                46,
+                47,
+                50,
+                53,
+                52,
+                42,
+                43,
+                44,
+                15,
+                16,
+                17,
+                24,
+                25,
+                26,
+                33,
+                34,
+                35,
+            ]
 
-    elif face == 'L':
-        src = [0, 3, 6, 18, 21, 24, 45, 48, 51, 38, 41, 44, 9, 10, 11, 12, 14, 15, 16, 17]
-        if direction == 'CW':
-            dst = [18, 21, 24, 45, 48, 51, 44, 41, 38, 6, 3, 0, 11, 14, 17, 10, 16, 9, 12, 15]
-        elif direction == 'CCW':
-            dst = [44, 41, 38, 0, 3, 6, 18, 21, 24, 51, 48, 45, 15, 12, 9, 16, 10, 17, 14, 11]
+    elif face == "L":
+        src = [
+            0,
+            3,
+            6,
+            18,
+            21,
+            24,
+            45,
+            48,
+            51,
+            38,
+            41,
+            44,
+            9,
+            10,
+            11,
+            12,
+            14,
+            15,
+            16,
+            17,
+        ]
+        if direction == "CW":
+            dst = [
+                18,
+                21,
+                24,
+                45,
+                48,
+                51,
+                44,
+                41,
+                38,
+                6,
+                3,
+                0,
+                11,
+                14,
+                17,
+                10,
+                16,
+                9,
+                12,
+                15,
+            ]
+        elif direction == "CCW":
+            dst = [
+                44,
+                41,
+                38,
+                0,
+                3,
+                6,
+                18,
+                21,
+                24,
+                51,
+                48,
+                45,
+                15,
+                12,
+                9,
+                16,
+                10,
+                17,
+                14,
+                11,
+            ]
 
-    elif face == 'R':
-        src = [2, 5, 8, 20, 23, 26, 47, 50, 53, 36, 39, 42, 27, 28, 29, 30, 32, 33, 34, 35]
-        if direction == 'CW':
-            dst = [42, 39, 36, 2, 5, 8, 20, 23, 26, 53, 50, 47, 29, 32, 35, 28, 34, 27, 30, 33]
-        elif direction == 'CCW':
-            dst = [20, 23, 26, 47, 50, 53, 42, 39, 36, 8, 5, 2, 33, 30, 27, 34, 28, 35, 32, 29]
+    elif face == "R":
+        src = [
+            2,
+            5,
+            8,
+            20,
+            23,
+            26,
+            47,
+            50,
+            53,
+            36,
+            39,
+            42,
+            27,
+            28,
+            29,
+            30,
+            32,
+            33,
+            34,
+            35,
+        ]
+        if direction == "CW":
+            dst = [
+                42,
+                39,
+                36,
+                2,
+                5,
+                8,
+                20,
+                23,
+                26,
+                53,
+                50,
+                47,
+                29,
+                32,
+                35,
+                28,
+                34,
+                27,
+                30,
+                33,
+            ]
+        elif direction == "CCW":
+            dst = [
+                20,
+                23,
+                26,
+                47,
+                50,
+                53,
+                42,
+                39,
+                36,
+                8,
+                5,
+                2,
+                33,
+                30,
+                27,
+                34,
+                28,
+                35,
+                32,
+                29,
+            ]
 
-    elif face == 'B':
-        src = [36, 37, 38, 41, 44, 43, 42, 39, 2, 1, 0, 9, 12, 15, 51, 52, 53, 35, 32, 29]
-        if direction == 'CW':
-            dst = [38, 41, 44, 43, 42, 39, 36, 37, 9, 12, 15, 51, 52, 53, 35, 32, 29, 2, 1, 0]
-        elif direction == 'CCW':
-            dst = [42, 39, 36, 37, 38, 41, 44, 43, 35, 32, 29, 2, 1, 0, 9, 12, 15, 51, 52, 53]
+    elif face == "B":
+        src = [
+            36,
+            37,
+            38,
+            41,
+            44,
+            43,
+            42,
+            39,
+            2,
+            1,
+            0,
+            9,
+            12,
+            15,
+            51,
+            52,
+            53,
+            35,
+            32,
+            29,
+        ]
+        if direction == "CW":
+            dst = [
+                38,
+                41,
+                44,
+                43,
+                42,
+                39,
+                36,
+                37,
+                9,
+                12,
+                15,
+                51,
+                52,
+                53,
+                35,
+                32,
+                29,
+                2,
+                1,
+                0,
+            ]
+        elif direction == "CCW":
+            dst = [
+                42,
+                39,
+                36,
+                37,
+                38,
+                41,
+                44,
+                43,
+                35,
+                32,
+                29,
+                2,
+                1,
+                0,
+                9,
+                12,
+                15,
+                51,
+                52,
+                53,
+            ]
 
-    elif face == 'F':
-        src = [18, 19, 20, 23, 26, 25, 24, 21, 6, 7, 8, 27, 30, 33, 47, 46, 45, 17, 14, 11]
-        if direction == 'CW':
-            dst = [20, 23, 26, 25, 24, 21, 18, 19, 27, 30, 33, 47, 46, 45, 17, 14, 11, 6, 7, 8]
-        elif direction == 'CCW':
-            dst = [24, 21, 18, 19, 20, 23, 26, 25, 17, 14, 11, 6, 7, 8, 27, 30, 33, 47, 46, 45]
+    elif face == "F":
+        src = [
+            18,
+            19,
+            20,
+            23,
+            26,
+            25,
+            24,
+            21,
+            6,
+            7,
+            8,
+            27,
+            30,
+            33,
+            47,
+            46,
+            45,
+            17,
+            14,
+            11,
+        ]
+        if direction == "CW":
+            dst = [
+                20,
+                23,
+                26,
+                25,
+                24,
+                21,
+                18,
+                19,
+                27,
+                30,
+                33,
+                47,
+                46,
+                45,
+                17,
+                14,
+                11,
+                6,
+                7,
+                8,
+            ]
+        elif direction == "CCW":
+            dst = [
+                24,
+                21,
+                18,
+                19,
+                20,
+                23,
+                26,
+                25,
+                17,
+                14,
+                11,
+                6,
+                7,
+                8,
+                27,
+                30,
+                33,
+                47,
+                46,
+                45,
+            ]
 
     temp = state.copy()
     for i, j in zip(src, dst):
         state[j] = temp[i]
 
+
 def recolor(gui, state, params):
-    '''Recolor the cube in the GUI.'''
+    """Recolor the cube in the GUI."""
 
     # Get graphics objects from GUI
     obj = gui.items
     squares = obj[:-1]
 
     # Extract relevant parameters
-    n = params['n']
-    c = params['colors']
+    n = params["n"]
+    c = params["colors"]
 
     # Update colors
     for i in range(len(state)):
         squares[i].setFill(c[state[i]])
 
+
 def simulate(state, node):
-    '''Simulate rotating the cube from an input state to determine resulting state. 
-    The input node is a sequence of rotations.'''
+    """Simulate rotating the cube from an input state to determine resulting state.
+    The input node is a sequence of rotations."""
     s = state.copy()  # copy the state so that we don't change the actual cube!
     # ***ENTER CODE HERE***  (4 lines)
+    for face in node:
+        rotate(s, face.upper(), "CCW" if face.isupper() else "CW")
 
     return s
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(parser.parse_args())
